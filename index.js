0245e8272989e22322420635d9a64ed91644e8e0
OwlBag <Owlbag0410@gmail.com>
2022-05-08 23:30:18 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const inquirer = require('inquirer');
+
+// TODO: Create an array of questions for user input
+const questions = [
+    "What is your project's name? (Required)",
+    "Describe your project (Required)",
+    "What are the steps to install your project? (Leave blank if there are none)",
+    "Provide instructions for using your project",
+    "Provide your contribution guidelines",
+    "Provide instructions to test your project"
+
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
